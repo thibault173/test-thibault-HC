6d62fd640fa406a8e87d066c8d626806a56a7632
thibaultvillard <thibault.villard@gmail.com>
2019-03-11 18:37:14 +0100

    Adding fields on users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_11_114043) do
+ActiveRecord::Schema.define(version: 2019_03_11_171950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,9 @@ ActiveRecord::Schema.define(version: 2019_03_11_114043) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "phone_number"
+    t.text "biography"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,10 +4,12 @@
 
 User.destroy_all
 
-
 user = User.create( email: 'tv@test.fr',
                     password: 'azerty',
-                    password_confirmation: 'azerty')
+                    password_confirmation: 'azerty',
+                    name: 'Thibault',
+                    phone_number: '07 01 01 01 01',
+                    biography: 'Après 7 ans de supply chain, je me mets au code !')
 
 puts "Creating #{User.all.size} users"
 

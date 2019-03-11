11d4eed265b9febb22793aec893588aca878a08d
thibaultvillard <thibault.villard@gmail.com>
2019-03-11 16:12:09 +0100

    Add SMTP settings and update app.json

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,19 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Setup the mailer config
   config.action_mailer.default_url_options = { host: 'http://test-thibault-hc-staging.herokuapp.com/' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'test-thibault-hc-staging.herokuapp.com',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
